df464d16b2a5d2208f67d883cff92de3b0b40a9a
Chrissy <chrissy.stroud@ou.edu>
2019-05-10 01:05:27 -0500

    added httpclient

--- Problem1-JSON/MainWindow.xaml.cs
+++ Problem1-JSON/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿//Chrissy Stroud - Final Exam//
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -24,7 +26,31 @@ namespace Problem1_JSON
         public MainWindow()
         {
             InitializeComponent();
-            
+            ClearAll();
         }
+
+        private void BtnGetStats_Click(object sender, RoutedEventArgs e)
+        {
+            List<Movie> MoviesNumberUsersVoted = new List<Movie>();
+            using (var client = new HttpClient())
+            {
+                var response = client.GetAsync(@"http://pcbstuou.w27.wh-2.com/webservices/3033/api/Movies?number=100").Result;
+                var content = response.Content.ReadAsStringAsync().Result;
+                var movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+            }
+
+        }
+        private void ClearAll()
+        {
+            txtIMDBscore.Inlines.Clear();
+            txtDirector.Inlines.Clear();
+            txtActor1.Inlines.Clear();
+            lstMovieGenre.Items.Clear();
+            lstVotedUsers.Items.Clear();
+
+
+        }
+        
+
     }
 }
