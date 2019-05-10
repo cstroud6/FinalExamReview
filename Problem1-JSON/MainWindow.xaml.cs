b9d0e8ecdf784bc841b9c1a5d43e0b922deb7a98
Chrissy <chrissy.stroud@ou.edu>
2019-05-10 07:28:19 -0500

    Completed question 1

--- Problem1-JSON/MainWindow.xaml.cs
+++ Problem1-JSON/MainWindow.xaml.cs
@@ -31,13 +31,29 @@ namespace Problem1_JSON
 
         private void BtnGetStats_Click(object sender, RoutedEventArgs e)
         {
+            //get the data 
             List<Movie> MoviesNumberUsersVoted = new List<Movie>();
+            List<Movie> highestIMDBScores = new List<Movie>();
+
+            List<Movie> movies;
             using (var client = new HttpClient())
             {
                 var response = client.GetAsync(@"http://pcbstuou.w27.wh-2.com/webservices/3033/api/Movies?number=100").Result;
                 var content = response.Content.ReadAsStringAsync().Result;
-                var movies = JsonConvert.DeserializeObject<List<Movie>>(content);
+                movies = JsonConvert.DeserializeObject<List<Movie>>(content);
             }
+            //then start answering questions
+            //1. List all of the different genres for the movies
+            foreach (var item in movies)
+            {
+                lstMovieGenre.Items.Add(item.genres);
+
+
+            }
+            //2. Which movie has the highest IMDB score ?
+            //3. List all of the different movies that have a number of voted users with 350000 or more
+            //4. How many movies where Anthony Russo is the director ?
+            //5. How many movies where Robert Downey Jr. is the actor 1 ?
 
         }
         private void ClearAll()
